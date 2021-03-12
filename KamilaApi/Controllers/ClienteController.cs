8393645490bfbcf1a8d78c92b201c0af5dbe3e89
kamil <kamila.nascimento@contractor-mc1.com>
2021-03-12 18:20:23 -0300

    iniciando

---
+++ KamilaApi/Controllers/ClienteController.cs
@@ -0,0 +1,33 @@
+﻿using KamilaApi.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KamilaApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ClienteController : ControllerBase
+    {
+        //public IActionResult Banana()
+        //{
+        //    return Ok("Banana");
+        //}
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var listaDeClientes = new List<Cliente>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                listaDeClientes.Add(new Cliente());
+            }
+
+            return Ok(listaDeClientes);
+        }
+    }
+}
---
+++ KamilaApi/Model/Cliente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KamilaApi.Model
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+        
+        public Cliente()
+        {
+            Random rd = new Random();
+            Id = rd.Next(1, 100);
+
+            Nome = $"Teste {Id}";
+        }
+    }
+}
